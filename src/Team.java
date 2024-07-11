5071812306f28ebdaa62ac6003ab340e4d6e9e79
goutham120203 <gouthampaladugu12@gmail.com>
2024-07-11 22:16:50 +0530

    project

---
+++ src/Team.java
@@ -0,0 +1,8 @@
+public class Team {
+    public void team2(){
+        System.out.println("Goutham");
+        System.out.println("sai");
+        System.out.println("venkat");
+        System.out.println("ritish");
+    }
+}
